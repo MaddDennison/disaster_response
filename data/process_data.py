7c2f17912be7dc10f8bc5a4cee8a367cc8ee12d6
Nathan Dennison <nathan.dennison@gmail.com>
2020-01-21 01:15:13 -0400

    Basic ETL objects created

--- data/process_data.py
+++ data/process_data.py
@@ -1,19 +1,61 @@
 import sys
-
+import pandas as pd
+import numpy as np
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+
+
+    '''
+    # load messages dataset
+    messages = pd.read_csv('messages.csv')
+    # load categories dataset
+    categories = pd.read_csv('categories.csv')
+    # merge datasets
+    df = messages.merge(categories, left_on='id', right_on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    '''
+    '''
+    #a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    #extract a list of new column names for categories.
+    row = categories.loc[:0]
+    category_colnames = list(row.apply(lambda x: str(x[0])[:-2]))
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1:]
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # drop the original categories column from `df`
+    df.drop(columns=['categories'], inplace=True)
+
+    # merge the original dataframe with the new `categories` dataframe
+    df = df.merge(categories, left_index=True, right_index=True)
+
+    # drop duplicates
+    df.drop_duplicates(inplace=True)
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    '''
+    pass
 
 
 def main():
+    '''
+    '''
+
     if len(sys.argv) == 4:
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
@@ -24,12 +66,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
